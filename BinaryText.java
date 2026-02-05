68eab8f4c816135c9bb382ab1d6280af55e3d860
Chaotic-V01D <bmyoungbauer@gmail.com>
2026-02-05 16:13:45 -0600

    first commit

---
+++ BinaryText.java
@@ -0,0 +1,7 @@
+package CSCI4041Challenge1;
+
+public class BinaryText {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+}
---
+++ Tree.java
@@ -0,0 +1,7 @@
+package CSCI4041Challenge1;
+
+public class Tree {
+    public Tree(){
+        
+    }
+}
