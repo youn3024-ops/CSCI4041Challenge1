0167e1c93fe45d2e1691133a43dc7abe581ef522
Chaotic-V01D <bmyoungbauer@gmail.com>
2026-02-06 11:18:40 -0600

    Basic binary tree implementation with Node methods (getters & setters) and basic tree insertion

--- BinaryText.java
+++ BinaryText.java
@@ -1,7 +1,14 @@
 package CSCI4041Challenge1;
 
+//Written by YOUN3024
+
 public class BinaryText {
     public static void main(String[] args){
         System.out.println("Hello, World!");
+
+        Tree myTree = new Tree();
+        Node myNode = new Node("Test", null, null);
+        myTree.insertNode(myNode);
+        System.out.println(myTree);
     }
 }
